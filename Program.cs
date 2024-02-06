c7f4fbeb664a116db6cbc0728f48b248c29b9dab
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-06 17:55:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ParamsKeyword
+{
+    public class NumberPrinter
+    {
+        public void PrintNumber(params int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberPrinter = new NumberPrinter();
+            numberPrinter.PrintNumber(new int[] {1,2,3,4,5});
+            Console.WriteLine();
+            numberPrinter.PrintNumber(1,3,4,5,100);
+
+            
+        }
+    }
+}
